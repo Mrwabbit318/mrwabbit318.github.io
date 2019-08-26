b76d2d6ef59df2016de011cf65c38360d35e1445
Mrwabbit318 <Abnerrod98@hotmail.com>
2019-08-25 22:15:38 -0400

    Update

---
+++ precache-manifest.aa61a80a38bb114efe253d976e9194cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b474ed79d5949f653ea",
+    "url": "/static/js/main.4b474ed7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4b474ed79d5949f653ea",
+    "url": "/static/css/main.8afde4da.chunk.css"
+  },
+  {
+    "revision": "0c1bce88a08c5b1f62aaad749e01ee30",
+    "url": "/index.html"
+  }
+];
